9542ecf3b6219623503c624f796c71b6d517ef59
ShibamD <>
2025-07-17 10:03:55 +0000

    Project init

---
+++ Impact-SPRING SESSIONs06/src/main/java/org/Impact-SPRING SESSIONs06/Application.java
@@ -0,0 +1,13 @@
+package org.Impact-SPRING SESSIONs06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
